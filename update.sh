52877f02c4b645b53f04c75e70b51111e6395df1
Amund Tenstad <github@amund.io>
2017-04-03 09:31:59 +0200

    Update git commands

--- update.sh
+++ update.sh
@@ -4,12 +4,12 @@ repo_root=$(awk -F': ' '/repo_root/ {print $2}' local_settings.txt)
 pyenv=$(awk -F': ' '/pyenv/ {print $2}' local_settings.txt)
 
 cd $repo_root
-sudo git pull
-sudo git reset --hard origin/master
+git reset --hard
+git checkout master
+git fetch --all
+git reset --hard origin/master
 sudo $pyenv manage.py makemigrations
 sudo $pyenv manage.py migrate
 sudo $pyenv manage.py collectstatic --noinput
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
-
-
